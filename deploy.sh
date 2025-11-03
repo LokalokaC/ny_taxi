64e54a51b9c3a3159031a260638a47c8655061ca
LokalokaC <janelle801020@hotmail.com>
2025-11-03 21:44:19 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,17 +6,21 @@ cd /opt/airflow
 echo "Configuring Docker authentication for Artifact Registry..."
 gcloud auth configure-docker us-west1-docker.pkg.dev --quiet
 
-if [ ! -f "initialization_done" ]; then
-    echo "Running Airflow initialization..."
-    docker compose -f docker-compose.yml --env-file .env run --rm --no-deps airflow-init
-    touch initialization_done
-fi
-
-echo "Pulling latest image..."
+echo "Pulling images and starting services..."
 docker compose -f docker-compose.yml --env-file .env pull
-
-echo "Starting services..."
 docker compose -f docker-compose.yml --env-file .env up -d --remove-orphans
+
+if [ ! -f "initialization_done" ]; then
+    echo "Running Airflow database migration and user creation..."
+    docker compose -f docker-compose.yml --env-file .env run --rm --no-deps airflow-init
+
+if [ $? -eq 0 ]; then
+        touch initialization_done
+        echo "Airflow initialization succeeded."
+    else
+        echo "Airflow initialization FAILED. Check logs."
+    fi
+fi
 
 echo "Pruning old images..."
 docker image prune -f
