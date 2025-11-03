b736a2fb00b358957ab4f4f73b6201a9f3ac677c
LokalokaC <janelle801020@hotmail.com>
2025-11-03 15:08:55 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,12 +8,12 @@ gcloud auth configure-docker "us-west1-docker.pkg.dev" --quiet
 
 if [ ! -f "initialization_done" ]; then
     echo "Running Airflow initialization..."
-    docker compose -f docker-compose.prod.yml --env-file .env run --rm --no-deps init
+    docker compose -f docker-compose.yml --env-file .env run --rm --no-deps init
     touch initialization_done
 fi
 
 echo "Pulling latest image..."
-docker compose -f docker-compose.prod.yml --env-file .env pull
+docker compose -f docker-compose.yml --env-file .env pull
 
 echo "Starting services..."
 docker compose -f docker-compose.yml --env-file .env up -d --remove-orphans
